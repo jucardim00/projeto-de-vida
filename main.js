0614b213cdf043fc063b65c66c82f673aced6e82
jucardim00 <cardim.julia@escola.pr.gov.br>
2026-04-22 16:20:32 -0300

    n

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const botoes=document.querySelectorAll(".botao");
-console.log(botoes);
+const botoes = document.querySelectorAll(".botao");
 
-for(let i=0; i<= botoes.length; i++){
-
-    console.log(botoes[i]);
-    botoes[i].onclick=function(){
-        botoes[i].classList.add("ativo");
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
     }
-    console.log(botoes[i]);
+
+    botoes[i].classList.add("ativo");
+  };
 }
